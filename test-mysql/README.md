2b00c7742c7d9328b5aff589c139b5b79408f06e
dilipul <likesdeepu@gmail.com>
2019-09-23 11:39:36 +0530

    test

---
+++ test-mysql/README.md
@@ -0,0 +1,4 @@
+# test-mysql
+
+TODO: Enter the cookbook description here.
+
---
+++ test-mysql/metadata.rb
@@ -0,0 +1,29 @@
+name 'test-mysql'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures test-mysql'
+long_description 'Installs/Configures test-mysql'
+version '0.1.0'
+chef_version '>= 14.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/test-mysql/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/test-mysql'
+
+name	'mysql'
+maintainer	'test_company'
+maintainer_email	'test@gmail.com'
+license	'all rights reserved'
+description	'Installs/Configues mysql'
+version	'0.1.0'
+
+depends 'apt'
